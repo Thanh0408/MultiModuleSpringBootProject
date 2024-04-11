0990831778b085d98f9e2f79095e6889408a8cf4
Thanh0408 <quangthanh.hust@gmail.com>
2024-04-12 01:06:25 +0700

    thanhlq: add Main class for Common and Authentication module, down java version from 11 to 8

---
+++ authentication-module/src/main/java/demo/com/vn/AuthenticationModule.java
@@ -0,0 +1,11 @@
+package demo.com.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationModule {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationModule.class, args);
+    }
+}
